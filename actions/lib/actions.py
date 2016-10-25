4950fbf3db755725f8f6346945a9e3dd4d3e7751
Kiran Ram <phxkiran@gmail.com>
2016-10-25 13:09:18 -0700

    flake8 and pylink warnings fixed

--- actions/lib/actions.py
+++ actions/lib/actions.py
@@ -1,15 +1,16 @@
 from st2actions.runners.pythonrunner import Action
 from zenpy import Zenpy
 
+
 class BaseAction(Action):
     def __init__(self, config):
         super(BaseAction, self).__init__(config)
         self.creds = {
-            'subdomain' : self.config['subdomain'],
+            'subdomain': self.config['subdomain'],
             'email': self.config['username'],
-            'token' : self.config['token']
+            'token': self.config['token']
         }
         self.client = self._get_client()
 
     def _get_client(self):
-        return Zenpy(instance_name, **self.creds)
+        return Zenpy(**self.creds)
